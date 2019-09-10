d9454afc194b455626f7d53df1df1f0f16f5234e
Henry <henry@kirkness.com>
2019-09-10 22:22:36 +0100

    fix: remove lib

--- lib/modal-controller.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import { ModalControllerProviderProps, ConsumerProps } from "./types";
-declare const ModalControllerProvider: (props: ModalControllerProviderProps) => JSX.Element;
-declare const useModalController: () => ConsumerProps;
-export { ModalControllerProvider, useModalController };
